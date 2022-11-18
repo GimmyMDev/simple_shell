a5983efe040142d629e5afd882a930e26c364279
Humera <humera210311@gmail.com>
2022-11-18 13:06:59 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -1,75 +1,48 @@
 #include "shell.h"
-
 /**
- * free_data - frees data structure
- * @datash: data structure
- * Return: no return
- */
-
-void free_data(data_shell *datash)
+*main - Entry point carrying out the read, execute then print of output loop
+*@ac: argument count
+*@av: argument vector
+*@envi: environment vector
+*Return: 0
+*/
+int main(int ac, char **av, char *envi[])
 {
-	unsigned int i;
-
-	for (i = 0; datash->_environ[i]; i++)
+	char *buf = NULL, *pathcmd = NULL, *path = NULL;
+	size_t buf_size = 0;
+	ssize_t line_size = 0;
+	char **cmd = NULL, **paths = NULL;
+	(void)envi, (void)av;
+	if (ac < 1)
+	return (-1);
+	signal(SIGINT, handle_signal);
+	while (1)
 	{
-		free(datash->_environ[i]);
+		free_buffers(cmd);
+		free_buffers(paths);
+		free(pathcmd);
+		prompt_user();
+		line_size = getline(&buf, &buf_size, stdin);
+		if (line_size < 0)
+			break;
+		info.ln_count++;
+		if (buf[line_size - 1] == '\n')
+		buf[line_size - 1] = '\0';
+		cmd = tokenizer(buf);
+		if (cmd == NULL || *cmd == NULL || **cmd == '\0')
+			continue;
+		if (checker(cmd, buf))
+			continue;
+		path = find_path();
+		paths = tokenizer(path);
+																		pathcmd = test_path(paths, cmd[0]);
+		if (!pathcmd)
+			perror(av[0]);
+		else
+		execution(pathcmd, cmd);
 	}
-
-	free(datash->_environ);
-	free(datash->pid);
-}
-
-
-/**
- * set_data - Initialize data structure
- * @datash: data structure
- * @av: argument vector
- * Return: no return
- */
-
-void set_data(data_shell *datash, char **av)
-{
-	unsigned int i;
-
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
-}
-
-
-/**
- * main - Entry point
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success.
- */
-
-int main(int ac, char **av)
-{
-	data_shell datash;
-	(void) ac;
-
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+	if (line_size < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
+	free(buf);
+	return (0);
 }
